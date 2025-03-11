658ba932dd043e2e99a4370b3ae8c67ded3e7802
justniar <salsaniarno@gmail.com>
2025-03-11 23:40:33 +0700

    feat: auth service

--- backend/services/auth_service.go
+++ backend/services/auth_service.go
@@ -1,11 +1,42 @@
 package services
 
-import "backend/repositories"
+import (
+	"backend/repositories"
+	"backend/utils"
+	"errors"
+)
 
 type AuthService struct {
-	r *repositories.Repository
+	Repo *repositories.Repository
 }
 
 func NewAuthService(r *repositories.Repository) *AuthService {
-	return &AuthService{Repo: repo}
+	return &AuthService{Repo: r}
+}
+
+func (s *AuthService) Register(user models.user) error {
+	hashedPassword, err := utils.HashPassword(user.PasswordHash)
+	if err != nil {
+		return err
+	}
+	user.PasswordHash = hashedPassword
+	return s.Repo.RegisterUser(user)
+}
+
+func (s *AuthService) Login(email, password string) (string, error) {
+	user, err := s.Repo.GetUserByEmail(email)
+	if err != nil {
+		return "", errors.New("user not found")
+	}
+
+	if !utils.CheckPassword(password, user.PasswordHash) {
+		return "", errors.New("invalid credentials")
+	}
+
+	token, err := utils.GenerateToken(user.ID)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
 }
