b3c3d7cf195360b58c751148037da6f6011c367a
justniar <salsaniarno@gmail.com>
2025-03-11 22:45:02 +0700

    add models

---
+++ backend/models/models.go
@@ -0,0 +1,36 @@
+package models
+
+import "time"
+
+type User struct {
+	ID           int       `json:"id"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updates_at"`
+	DeletedAt    time.Time `json:"deleted_at"`
+}
+
+type Book struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Author    string    `json:"author"`
+	Stock     int       `json:"stock"`
+	ImageUrl  string    `json:"image_url"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updates_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
+
+type BorrowHistory struct {
+	ID         int        `json:"id"`
+	UserID     int        `json:"user_id"`
+	BookID     int        `json:"book_id"`
+	BorrowDate time.Time  `json:"borrow_date"`
+	ReturnDate *time.Time `json:"return_date"`
+	Status     string     `json:"status"`
+	CreatedAt  time.Time  `json:"created_at"`
+	UpdatedAt  time.Time  `json:"updates_at"`
+	DeletedAt  time.Time  `json:"deleted_at"`
+}
