aa4e5cd44536d67168b55a89e4b2a36c1da0b4f4
justniar <salsaniarno@gmail.com>
2025-03-20 21:50:29 +0700

    fix: navbar

--- front-end/src/components/organism/navbar/Navbar.tsx
+++ front-end/src/components/organism/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { IoMdLogOut } from "react-icons/io";
 import { useRouter } from "next/navigation"; 
@@ -7,12 +7,19 @@ interface NavbarProps {
   username: string;
 }
 
-const Navbar: React.FC<NavbarProps> = ({ username }) => {
+const Navbar: React.FC<NavbarProps> = () => {
   const router = useRouter();
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("username");
+    setUsername(storedUser);
+  }, []);
 
   const handleLogout = () => {
     if (confirm("Yakin ingin logout?")) {
       localStorage.removeItem("token");
+      localStorage.removeItem("username");
       router.push("/auth/login");
     }
   };
@@ -21,7 +28,7 @@ const Navbar: React.FC<NavbarProps> = ({ username }) => {
     <nav className="w-full h-15 bg-amber-50 p-4 shadow-md">
       <div className="container mx-auto flex justify-end items-center">
         <div className="flex items-center text-red-900 cursor-pointer space-x-3">
-          <span className="font-semibold">{username}</span>
+          <span className="font-semibold">{username || "Guest"} </span>
           <FaRegCircleUser className="w-6 h-6" />
           <button onClick={handleLogout} className="hover:text-red-950">
             <IoMdLogOut className="w-6 h-6" />
